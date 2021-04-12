8c876dc3f78320026690a37c2da0abcc9a98a4d2
David Nguyen <david_nguyen@sunsuper.com.au>
2021-04-12 22:39:09 +1000

    Sync products from all banks

--- firebase/products-sync/functions/index.js
+++ firebase/products-sync/functions/index.js
@@ -7,32 +7,37 @@ exports.productSync = functions
   .region("australia-southeast1")
   .pubsub.schedule("every 2 hours")
   .onRun(async (context) => {
+    axios.defaults.headers.common["x-v"] = "2";
     try {
-      axios.defaults.headers.common["x-v"] = "2";
-      const response = await axios.get("https://api.commbank.com.au/public/cds-au/v1/banking/products");
-      const products = response.data.data.products;
-      if (products.length && products.length > 0) {
-        await Promise.all(products.map(async (p) => {
-          const productRef = admin.firestore().collection("products")
-            .doc(p.productId);
-          const snapshot = await productRef.get();
-          if (snapshot.exists) {
-            const storedProduct = snapshot.data();
-            if (p.lastUpdated !== storedProduct.lastUpdated) {
-              await productRef.set(p);
-              console.log(`Product '${p.name}' (${p.productId}) is updated`);
+      const banksQuery = await admin.firestore().collection("banks").get();
+      await Promise.all(banksQuery.docs.map(async (bankSnapshot) => {
+        const bank = bankSnapshot.data();
+        console.log(`Sync products from ${bank.name}`);
+        const response = await axios.get(`${bank.apiBaseUrl}/products`);
+        const products = response.data.data.products;
+        if (products.length && products.length > 0) {
+          await Promise.all(products.map(async (p) => {
+            const productRef = admin.firestore().collection("products")
+              .doc(p.productId);
+            const snapshot = await productRef.get();
+            if (snapshot.exists) {
+              const storedProduct = snapshot.data();
+              if (p.lastUpdated !== storedProduct.lastUpdated) {
+                await productRef.set(p);
+                console.log(`Product '${p.name}' (${p.productId}) is updated`);
+              } else {
+                console.log(`Product '${p.name}' (${p.productId}) is skipped, no changes since last update`);
+              }
             } else {
-              console.log(`Product '${p.name}' (${p.productId}) is skipped, no changes since last update`);
+              await productRef.set(p);
+              console.log(`Product '${p.name}' (${p.productId}) is added`);
             }
-          } else {
-            await productRef.set(p);
-            console.log(`Product '${p.name}' (${p.productId}) is added`);
-          }
-        }));
-      } else {
-        console.log("Unable to parse products from api");
-      }
+          }));
+        } else {
+          console.error("Unable to parse products from api");
+        }
+      }));
     } catch (error) {
-      console.log("There was an error while trying to sync products");
+      console.error("There was an error while trying to sync products", error);
     }
   });
