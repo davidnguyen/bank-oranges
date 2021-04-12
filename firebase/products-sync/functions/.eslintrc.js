3af7c266895f38381bdbc5c030b0f2ae6944fe16
David Nguyen <david_nguyen@sunsuper.com.au>
2021-04-12 21:48:11 +1000

    Add basic product sync for CBA

--- firebase/products-sync/functions/.eslintrc.js
+++ firebase/products-sync/functions/.eslintrc.js
@@ -4,12 +4,16 @@ module.exports = {
     es6: true,
     node: true,
   },
+  parserOptions: {
+    ecmaVersion: 8,
+  },
   extends: [
     "eslint:recommended",
     "google",
   ],
   rules: {
-    quotes: ["error", "double"],
-    indent: ["error", 2, {MemberExpression: 1}],
+    "quotes": ["error", "double"],
+    "indent": ["error", 2, {MemberExpression: 1}],
+    "max-len": "off",
   },
 };
--- firebase/products-sync/functions/index.js
+++ firebase/products-sync/functions/index.js
@@ -1,22 +1,38 @@
 const functions = require("firebase-functions");
 const axios = require("axios");
-
-exports.helloWorld = functions
-  .region("australia-southeast1")
-  .https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
-  });
+const admin = require("firebase-admin");
+admin.initializeApp();
 
 exports.productSync = functions
   .region("australia-southeast1")
-  .pubsub.schedule("every 30 minutes")
-  .onRun((context) => {
-    axios.defaults.headers.common["x-v"] = "2";
-    axios.get("https://api.commbank.com.au/public/cds-au/v1/banking/products")
-      .then(function(response) {
-        console.log("Found some data");
-      })
-      .catch(function(error) {
-        console.log("There was an error while trying to sync products");
-      });
+  .pubsub.schedule("every 2 hours")
+  .onRun(async (context) => {
+    try {
+      axios.defaults.headers.common["x-v"] = "2";
+      const response = await axios.get("https://api.commbank.com.au/public/cds-au/v1/banking/products");
+      const products = response.data.data.products;
+      if (products.length && products.length > 0) {
+        await Promise.all(products.map(async (p) => {
+          const productRef = admin.firestore().collection("products")
+            .doc(p.productId);
+          const snapshot = await productRef.get();
+          if (snapshot.exists) {
+            const storedProduct = snapshot.data();
+            if (p.lastUpdated !== storedProduct.lastUpdated) {
+              await productRef.set(p);
+              console.log(`Product '${p.name}' (${p.productId}) is updated`);
+            } else {
+              console.log(`Product '${p.name}' (${p.productId}) is skipped, no changes since last update`);
+            }
+          } else {
+            await productRef.set(p);
+            console.log(`Product '${p.name}' (${p.productId}) is added`);
+          }
+        }));
+      } else {
+        console.log("Unable to parse products from api");
+      }
+    } catch (error) {
+      console.log("There was an error while trying to sync products");
+    }
   });
